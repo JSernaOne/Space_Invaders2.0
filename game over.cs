9e5fc31d57793eca04635ef0d8236300906bfe6a
johnatan.diosa@correounivalle.edu.co <Usuario@DESKTOP-KT0U8QS>
2022-11-20 18:18:40 -0500

    nuevo form del game over

---
+++ game over.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Space_Invaders2._0
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        private void Form2_load(object sender, EventArgs e)
+        {
+            pictureBox1.Location=new Point(330, 82);
+            button1.Location = new Point(154, 263);
+            
+
+        }
+        private void Form2_load(object sender, KeyEventArgs e)//INSTANCIAR EL FORM DE GAME OVER
+        {
+            Form2 gameover = new Form2();
+            gameover.ShowDialog();
+            this.Hide();
+        }
+        private void intentarlo()//BOTON DE PARA VOLVER A JUGAR (un nuevo intento)
+        {
+            Space_Invaders space_Invaders=new Space_Invaders();
+            space_Invaders.Show();
+            this.Hide();
+
+        }
+
+        private void salir()//  BOTON DE SALIR AL MENU EN EL GAME OVER
+        {
+            menu menu=new menu();//INSTANCIAEL FORM DE EL MENU
+            menu.Show();
+            this.Hide();
+            
+
+        }
+    }
+}
--- menu.Designer.cs
+++ menu.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Space_Invaders2._0
 {
-    partial class Form1
+    partial class menu
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(menu));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
